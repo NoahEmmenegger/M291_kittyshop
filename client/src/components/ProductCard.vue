8c52988844f37bde6385d3f7018447c6a64b290f
noah.emmenegger <noah.emmenegger@internezzo.ch>
2022-06-23 11:39:29 +0200

    Add product card

---
+++ client/src/components/ProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="product">product title: {{ product.title }}</div>
+</template>
+
+<script>
+export default {
+    name: 'ProductCard',
+    props: {
+        product: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
--- client/src/components/Products.vue
+++ client/src/components/Products.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="products">
         products
-        <tr v-for="(products, idx) in products" :key="idx">
-            <td>tset</td>
+        <tr v-for="(product, idx) in products" :key="idx">
+            <ProductCard :product="product" />
         </tr>
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import ProductCard from './ProductCard.vue';
+
 export default {
     name: 'Products',
+    computed: {
+        ...mapState({
+            products: 'products',
+        }),
+    },
+    components: {
+        ProductCard,
+    },
 };
 </script>
